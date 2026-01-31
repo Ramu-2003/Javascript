f91df5c6e6ca7ce67bc3ffc924dd987aa9559eaf
G RAMA KRISHNA PRASAD <132869239+Ramu-2003@users.noreply.github.com>
2026-01-31 23:03:29 +0530

    javascript

---
+++ JS-1-2.js
@@ -0,0 +1,18 @@
+//  javascript basics lesson 1.2 - HW
+
+//  1.f document.body.innerHTML to display good morning
+document.body.innerHTML = 'Good Morning';
+
+//  1.g display your name on the web page
+document.body.innerHTML = 'Rama Krishna';
+
+//  1h 
+//  10 + 8 + 20 => 38
+
+// 1i
+//  100 -20 -50 +200 => 230
+
+// 1j
+document.body.innerHTML = ' ';
+
+// exercise 1.2 png is completed these are the answers
