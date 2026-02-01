46c6831181b6df703d357fc0e48c199c1b75a0c1
G RAMA KRISHNA PRASAD <132869239+Ramu-2003@users.noreply.github.com>
2026-02-01 21:52:04 +0530

    JAVASCRIPT

---
+++ JS-3-1.js
@@ -0,0 +1,16 @@
+//  javascript basics lesson 3.1 - HW
+
+//  answers of 3.1 exercise  , questions on 3.i png
+
+// 3.a WelcometoJavascript
+
+//  3.b The total is:105
+
+//  3.c 'Total:15'
+
+//  3.d Result is 15
+
+// 3.e 1) The price is50rupees
+    //    2) The price is 50 rupees
+
+// exercise 3.1 png is completed these are the answers
