a2c1c1397f045319266e04353e6149e739c412e0
G RAMA KRISHNA PRASAD <132869239+Ramu-2003@users.noreply.github.com>
2026-02-01 21:47:40 +0530

    JAVASCRIPT

---
+++ JS-2-1.js
@@ -0,0 +1,24 @@
+//  javascript basics lesson 2.1 - HW
+
+// 2.a total price: 2 markers 25 and 1 notebook 60 rs
+//  "total price = (2*25+1*60) "
+
+// 2.b adding a delivery 40 rs to total 180 ?
+//  total = (180+40)
+
+//  2.c 3 lamps 320 rs and 3 extensions 450 with 60 delivery and 10% tax
+//   ((3*320 )+(450*2)+60) => 640 + 900 + 60 => 1600
+//  1600/10 or 1600 *0.1 = 160 
+//  1600 + 160 => 1760 
+
+// 2d 4 wall poster 150 rs and a frame 900 applies discount 100 add delivery 50 tax 10%
+//  ((4*150)+900-100+50) =>1450
+//  1450/10 or 1450*0.1 = 145
+//  1450 + 145 => 1595
+
+// 2 e 2100 is total tax is 10 % dleivery is 70 and a 200 discount before tax what is final amount
+//  (2100 +70 -200) => 1970
+//  1970 * 0.1 => 197
+//  1970 + 197 = 2167
+
+// exercise 2.1 png is completed these are the answers
