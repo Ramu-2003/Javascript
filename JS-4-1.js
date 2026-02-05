86909badf3f86743ad94a8f494634c04f145996c
G RAMA KRISHNA PRASAD <132869239+Ramu-2003@users.noreply.github.com>
2026-02-05 22:21:07 +0530

    JAVASCRIPT

---
+++ JS-4-1.js
@@ -0,0 +1,10 @@
+//  javascript basics lesson 4.1 - HW
+
+// 4.a <button> Subscribe </button>
+
+// 4.b <button> ramu </button> 
+    //    <button>Chicken</button>
+
+// 4.c <button class ="btn" onclick ="alert('Subscribed');">Subscribe </button>
+
+// exercise 4.1 png is completed these are the answers
