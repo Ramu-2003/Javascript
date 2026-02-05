1f3a7e6ab583ed2a42d86133f99d0bb4a903523f
G RAMA KRISHNA PRASAD <132869239+Ramu-2003@users.noreply.github.com>
2026-02-05 22:18:52 +0530

    JAVASCRIPT

---
+++ JS-3-3.js
@@ -0,0 +1,13 @@
+//  javascript basics lesson 3.3 - HW
+
+//  calculate the total price
+// 115+40+15 =170
+
+// 115 +40 => 155
+
+// 170
+
+// 155 / 20  => 31 
+// 155+31 =186
+
+// exercise 3.3 png is completed these are the answers
