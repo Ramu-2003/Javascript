9cdf1433542914117d96adfcd96f870b59a5dbee
G RAMA KRISHNA PRASAD <132869239+Ramu-2003@users.noreply.github.com>
2026-02-05 22:18:18 +0530

    JAVASCRIPT

---
+++ JS-3-2.js
@@ -0,0 +1,18 @@
+//  javascript basics lesson 3.2 - HW
+
+// `10 + 20 = ${"10"+20}`
+//  10+20 =1020 answer
+
+// `Sum: ${5+"5"}` vs `Sum: ${5+5}`
+    // Sum: 55 , Sum: 10  answer
+
+    // `Value: ${String(100)+50}`
+    //      Value: 10050 answer
+
+    // "Hello" + (5+5)
+    // Hello10 answer
+
+    // "Hello" + 5+5
+    // Hello55 answer
+
+// exercise 3.2 png is completed these are the answers
