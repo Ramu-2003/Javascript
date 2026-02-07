dacd4aa4ddf6c5233aa80add49e921376689fcf0
G RAMA KRISHNA PRASAD <132869239+Ramu-2003@users.noreply.github.com>
2026-02-07 20:57:47 +0530

    JAVASCRIPT

---
+++ JS-5-1.js
@@ -0,0 +1,16 @@
+//  javascript basics lesson 5.1 - HW
+
+// 5.a <script>
+    //    let name = "rama krishna";
+
+// 5.b console.log("My name is:",name);
+
+// 5.c  let total_cost = ((1*50)+(2*30)+(1*90));
+
+
+//  5.d console.log(total_cost);
+
+
+// 5.e let cost = (0.1 * total_cost);
+
+// exercise 5.1 png is completed these are the answers
