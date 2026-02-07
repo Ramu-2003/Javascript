7fcbc33249df2d50f2448ef65c50f56bf420bcfc
G RAMA KRISHNA PRASAD <132869239+Ramu-2003@users.noreply.github.com>
2026-02-07 20:58:16 +0530

    JAVASCRIPT

---
+++ JS-5-2.js
@@ -0,0 +1,83 @@
+//  javascript basics lesson 5.2 - HW
+
+// 5.f display 10% ta using ``
+//  `Tax(10%): ${tax}`
+    //  `TextTrack(10%): ${1770 *0.1}`
+
+    // 5.g let Total_cost = cost + Tax 
+    //  console.log(Total_cost);
+
+//  5.h   <!DOCTYPE html>
+// <html>
+//     <head>
+//         <title>mini project</title>
+//     </head>
+//     <body>
+//         <button onclick="
+//             console.log(`Cart Quantity: ${Quantity}`);
+//             ">Show Quantity</button>
+//         <button onclick="
+//             Quantity =  Quantity +1;
+//             console.log('Cart Quantity:',Quantity)
+//         ">Add to Cart</button>
+//         <button onclick="
+//             Quantity =  Quantity +2;
+//             console.log('Cart Quantity:',Quantity);
+//         ">+2</button>
+//         <button onclick="
+//             Quantity =  Quantity +3;
+//             console.log('Cart Quantity:',Quantity);
+//         ">+3</button>
+            // <button onclick="
+            //     Quantity += 4;
+            //     console.log('Cart Quantity:',Quantity);
+
+            // "></button>
+            // <button onclick="
+            //     Quantity += 5;
+            //     console.log('Cart Quantity:',Quantity);
+
+            // "></button>
+//         <button onclick="
+//             Quantity =  Quantity * 0;
+//             console.log('Cart Was Reset:',Quantity);
+//         ">Reset Cart</button>
+//         <script>
+//             let Quantity = 0;
+//         </script>
+//     </body>
+// </html>
+
+//  5 i
+/* <button onclick="
+    Quantity = cartquantity-1;
+    console.log("Remove Cart :",Quantity);
+">Remove Cart</button> */
+
+    // 5.j  <button onclick="
+    //   cartQuantity = cartQuantity - 2;
+    //   console.log(`Cart quantity: ${cartQuantity}`);
+    // ">-2</button>
+
+    // <button onclick="
+    //   cartQuantity = cartQuantity - 3;
+    //   console.log(`Cart quantity: ${cartQuantity}`);
+    // ">-3</button>
+
+    /* <button onclick="
+        cartquantity --;
+    console.log("Remove Cart :",Quantity);
+">Remove Cart</button>
+
+    5.j  <button onclick="
+      cartQuantity -= 2 ;
+      console.log(`Cart quantity: ${cartQuantity}`);
+    ">-2</button>
+
+    <button onclick="
+      cartQuantity -= 3;
+      console.log(`Cart quantity: ${cartQuantity}`);
+    ">-3</button>*/
+
+
+// exercise 5.2 png is completed these are the answers
