c0c788b0bb7ff3d2252f8b2776301eab5a5c30f7
G RAMA KRISHNA PRASAD <132869239+Ramu-2003@users.noreply.github.com>
2026-02-08 22:41:23 +0530

    JAVASCRIPT

---
+++ JS-6-1.js
@@ -0,0 +1,26 @@
+//  javascript basics lesson 6.1 - HW
+
+// 6.a
+currentTemparature = 27;
+if (currentTemparature >= 25){
+console.log("its warm");
+}else{
+    console.log("its cold")
+}
+
+//  6.b
+personage = 22;
+if (personage > 18) {
+    console.log("you are eligible for vote");
+}else {
+    console.log("you are not eligible for vote");
+}
+
+// 6.c
+
+PremiumMember = 200;
+customerTotalSpent = 100;
+
+
+
+// exercise 6.1 png is completed these are the answers
